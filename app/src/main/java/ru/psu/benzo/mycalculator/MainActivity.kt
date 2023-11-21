5cbd48fb7e8baecd5804cd657c7249c943432914
BlueQueen <dethwingomg@gmail.com>
2023-11-21 23:31:11 +0500

    Calculator. Математика без View binding.

--- app/src/main/java/ru/psu/benzo/mycalculator/MainActivity.kt
+++ app/src/main/java/ru/psu/benzo/mycalculator/MainActivity.kt
@@ -2,15 +2,33 @@ package ru.psu.benzo.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textViewOutput : TextView
+    lateinit var editTextValue1 : EditText
+    lateinit var editTextValue2 : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val x = 0
-        val y = 5
-
+        textViewOutput = findViewById(R.id.textViewOutput)
+        editTextValue1 = findViewById(R.id.editTextValue1)
+        editTextValue2 = findViewById(R.id.editTextValue2)
+        textViewOutput.text = "11111"
 
     }
+    fun onButtonPlusClick(view : View)
+    {
+        val val1 = editTextValue1.text.toString().toDouble()
+        val val2 = editTextValue2.text.toString().toDouble()
+        textViewOutput.text = ""+(val1+val2)
+        //textViewOutput.text = editTextValue1.text //.toString()
+        //textViewOutput.text = "2222222"
+    }
+
+
 }
